54259421d994b4fa20631f6edb1a57293321e41c
LalithaV95 <30973181+LalithaV95@users.noreply.github.com>
2017-08-15 17:38:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,82 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+    
+ articleOne:{
+    title: 'article-one |Lalli',
+    heading: 'Article-one',
+    date:'Aug 15,2017',
+    content:` <p>
+           This is the first article.
+            </p>`
+},
+ articleTwo:{
+    title: 'article-Two |Lalli',
+    heading: 'Article-Two',
+    date:'Aug 5,2017',
+    content:` <p>
+           This is the second article.
+            </p>`
+    
+    
+},
+articleThree:{
+    title: 'article-Three |Lalli',
+    heading: 'Article-Three',
+    date:'Aug 5,2017',
+    content:` <p>
+           This is the third article.
+            </p>`
+     
+}
+};
+function createTemplate(data) {
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    
+var htmpTemplate=`
+ <html>
+<head>
+    <title>${title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+ <b>
+   <div class="container">
+       <div>
+           <a href="/">Home</a>   
+       </div>
+       <div>
+         <p>
+           ${heading}
+         </p>
+        </div>
+        <div>
+           <p>
+          ${date} 
+           </p>
+        </div>
+        <div>
+            <p>
+           ${content}
+            </p>
+        </div>   
+           
+    </div>
+    </b>
+</body>
+
+</html>
+
+
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
