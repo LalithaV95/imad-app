a5797528f2c9e449ac704607a7e338e0f1f7ccca
LalithaV95 <30973181+LalithaV95@users.noreply.github.com>
2017-08-15 17:42:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,8 +94,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-
+   res.send(createTemplate(articleOne));
 });
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
