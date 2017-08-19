b142d5413a64da5c3685b7ee7da00e4a053e45fd
LalithaV95 <30973181+LalithaV95@users.noreply.github.com>
2017-08-19 12:35:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,8 @@
 console.log('Loaded!');
 console.log('To check whether things are working fine via logs')
+
+var element = document.getElementById('id1');
+element.innerHTML='Content Changed! Wow!'
+
+
+
