a26f1684313dd0fea8462118f2bb6b665a96f5de
LalithaV95 <30973181+LalithaV95@users.noreply.github.com>
2017-08-19 18:02:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,3 +37,18 @@ request.onreadystatechange = function(){
 request.open('GET','http://lalithambigav.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick=function(){
+    var names=["Name1","Name2","Name3","Name4"];
+    var list=[];
+    for(i=0;i<names.length;i++){
+        list+= '<li>'+ names[i] + '</li>';
+        
+    }
+    var ul=document.getElementById("NameList");
+    ul.innerHTML=list;
+    
+};
