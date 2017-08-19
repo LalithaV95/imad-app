17d04c89a74e44b7239c4d1dc8aa41317a0388d6
LalithaV95 <30973181+LalithaV95@users.noreply.github.com>
2017-08-19 13:04:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,8 +6,8 @@ element.innerHTML='Content Changed! Wow!';
 
 var img=document.getElementById('madi');
 function moveRight(){
-    marginLeft=marginLeft +5;
-    img.style.marginLeft=marginLeft +'px';
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft= marginLeft ='px';
 }
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
