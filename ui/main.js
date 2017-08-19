0d9da586ded3a9560323301ca2f3d7267bfd216c
LalithaV95 <30973181+LalithaV95@users.noreply.github.com>
2017-08-19 14:52:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,6 +19,6 @@ var counter=0;
 
 button.onclick=function(){
     counter=counter+1;
-    var span=span.getElementById("count");
+    var span=document.getElementById("count");
     span=counter.toString();
 };
