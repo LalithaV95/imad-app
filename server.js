873c885f12561d19e939c4166a3fd22f53895960
LalithaV95 <30973181+LalithaV95@users.noreply.github.com>
2017-08-19 11:14:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -91,6 +91,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 
 app.get('/:articleName',function(req,res){
