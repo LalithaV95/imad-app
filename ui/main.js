aa9dc245b3fadb4c244e3b1ee867d689b66e6eb9
LalithaV95 <30973181+LalithaV95@users.noreply.github.com>
2017-08-19 12:39:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,13 @@ console.log('Loaded!');
 console.log('To check whether things are working fine via logs')
 
 var element = document.getElementById('id1');
-element.innerHTML='Content Changed! Wow!'
+element.innerHTML='Content Changed! Wow!';
+
+var madiImg=document.getElementById('madi');
+madiImg.onclick= function() {
+    madiImg.style.moveLeft='200px';
+    
+};
 
 
 
