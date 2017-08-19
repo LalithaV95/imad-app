cf6129bd31287651cdc157583f608eaf6f85519b
LalithaV95 <30973181+LalithaV95@users.noreply.github.com>
2017-08-19 15:45:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,22 @@ img.onclick=function(){
 var button=document.getElementById("counter");
 var counter=0;
 
-button.onclick=function(){
+/*button.onclick=function(){
     counter=counter+1;
     var span=document.getElementById("count");
     span.innerHTML=counter.toString();
+};*/
+
+button.onclick=function(){
+  var request=new XMLHttpRequest();
+  
+request.onreadystatechange = function(){
+ if(request.readystate == XmlHttpRequest.DONE){
+     if(request.status==200){
+         var counter=request.responseText;
+         var span=document.getElementById("count");
+         span=counter.toString();
+     }
+ }   
+};
 };
